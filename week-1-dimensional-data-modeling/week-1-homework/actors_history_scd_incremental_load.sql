f486a2df5b74a32d4f2ad45543117a228c95b737
Kevin Dayno <kdayno@gmail.com>
2024-12-01 16:06:33 -0500

    Update week 1 homework

--- week-1-dimensional-data-modeling/week-1-homework/actors_history_scd_incremental_load.sql
+++ week-1-dimensional-data-modeling/week-1-homework/actors_history_scd_incremental_load.sql
@@ -1,43 +1,98 @@
-/** Query to Load Type 2 SCD **/
+/** Query to Execute Incremental Load into actors_history_scd (SCD2) **/
 -- INSERT INTO actors_history_scd
-WITH with_previous AS (
-    SELECT
-        actor,
-        to_date(year::varchar, 'yyyy') AS start_date,
-        (to_date(year::varchar, 'yyyy') + interval '1 year' - interval  '1 day')::date AS end_date,
-        quality_class,
-        is_active,
-        LAG(quality_class, 1) OVER (PARTITION BY actor ORDER BY year) AS previous_quality_class,
-        LAG(is_active, 1) OVER (PARTITION BY actor ORDER BY year) AS previous_is_active
-
-    FROM actors
-    WHERE year <= 2002
+WITH last_year_scd AS(
+    SELECT *
+    FROM actors_history_scd
+    WHERE start_date = '2020-01-01'
+        AND end_date = '2020-12-31'
     ),
 
-    with_indicators AS (
-    SELECT
-        *,
-        CASE
-            WHEN quality_class <> previous_quality_class THEN 1
-            WHEN is_active <> previous_is_active THEN 1
-            ELSE 0
-        END AS change_indicator
-    FROM with_previous
+    historical_scd AS (
+        SELECT *
+        FROM actors_history_scd
+        WHERE end_date < '2020-01-01'
+
     ),
 
-    with_streaks AS (
-    SELECT
-        *,
-        SUM(change_indicator) OVER (PARTITION BY actor ORDER BY start_date) AS streak_identifier
-    FROM with_indicators
+    this_year_data AS(
+        SELECT
+            *,
+            to_date(year::varchar, 'yyyy') AS start_date,
+            (to_date(year::varchar, 'yyyy') + interval '1 year' - interval  '1 day')::date AS end_date
+        FROM actors
+        WHERE year = '2021'
+    ),
+
+    unchanged_records AS (
+        SELECT
+            ty.actor,
+            ty.quality_class, ty.is_active,
+            ly.start_date,
+            ty.end_date
+        FROM this_year_data ty
+        INNER JOIN last_year_scd ly ON ty.actor = ly.actor
+        WHERE ty.quality_class = ly.quality_class
+            AND ty.is_active = ly.is_active
+    ),
+
+    changed_records AS (
+        SELECT
+            ty.actor,
+            unnest(
+                ARRAY[
+                    ROW(
+                        ly.quality_class,
+                        ly.is_active,
+                        ly.start_date,
+                        ly.end_date
+                        )::scd_type,
+                    ROW(
+                        ty.quality_class,
+                        ty.is_active,
+                        ty.start_date,
+                        ty.end_date
+                        )::scd_type]) AS records
+        FROM this_year_data ty
+        INNER JOIN last_year_scd ly ON ty.actor = ly.actor
+        WHERE (ty.quality_class <> ly.quality_class
+            OR ty.is_active <> ly.is_active)
+    ),
+
+    unnested_changed_records AS (
+        SELECT
+            actor,
+            (records::scd_type).quality_class,
+            (records::scd_type).is_active,
+            (records::scd_type).start_date,
+            (records::scd_type).end_date
+        FROM changed_records
+    ),
+
+    new_records AS (
+        SELECT
+            ty.actor,
+            ty.quality_class,
+            ty.is_active,
+            ty.start_date,
+            ty.end_date
+        FROM this_year_data ty
+        LEFT JOIN last_year_scd ly ON ty.actor = ly.actor
+        WHERE ly.actor IS NULL
     )
 
-SELECT
-    actor,
-    quality_class,
-    is_active,
-    MIN(start_date) AS start_date,
-    MAX(end_date) AS end_date
-FROM with_streaks
-GROUP BY actor, streak_identifier, is_active, quality_class
-ORDER BY actor, streak_identifier;
+    SELECT * FROM historical_scd
+
+    UNION ALL
+
+    SELECT * FROM unchanged_records
+
+    UNION ALL
+
+    SELECT * FROM unnested_changed_records
+
+    UNION ALL
+
+    SELECT * FROM new_records;
+
+
+
